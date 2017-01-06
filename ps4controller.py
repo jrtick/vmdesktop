c63bbe84e4a17b41083a8923813b1765c2c18fb1
jrtick <jrtick@andrew.cmu.edu>
2017-01-06 02:52:19 -0500

    Fully functional ps4 controller library

--- ps4controller.py
+++ ps4controller.py
@@ -1,8 +1,11 @@
 ##This file is inspired by
 ##https://gist.github.com/claymcleod/028386b860b75e4f5472
+#works with python AND python3, assuming you have the below libraries
 
-import pygame,os
-import time,threading
+import pygame #used for recognizing ps4 controller as a joystick
+import os #used for printing realtime controller setup
+import time #used for timing stats and polls
+import threading #used for monitoring controller in background
 
 #whether this is a library or a test
 run_as_app = (__name__ == "__main__")
@@ -15,6 +18,8 @@ class PS4Controller(object):
 
   def __init__(self,controllerID=0,displayInfo=False):
     self.thread = None #not running in background
+    self.killRequest = False #can request to stop a bg thread
+
     if(displayInfo):
       print("initializing controller...")
     #setup pygame
@@ -27,18 +32,20 @@ class PS4Controller(object):
       self.controllerID=controllerID
       if(not self.joystick.get_init()):
         self.joystick.init()
+    else:
+      print("Error. Could not find PS4 controller. Is your controller attached via USB to your computer?")
     
     #create a PS4 controller specific button map
     #arrows are mapped to the pygame joystick hat
     #bumpers also have an axis associated to them
     #no touchpad support that I know of (google?)
-    self.buttonValues = dict()
     self.buttonMap = ["square","x","circle","triangle","left-trigger","right-trigger","left-bumper","right-bumper","share","options","left-press","right-press","power","touchpad","left","right","up","down"]
+    self.buttonValues = dict()
     for button in self.buttonMap:
       self.buttonValues[button] = False #not pressed
 
-    self.axisValues = dict()
     self.axisMap = ["left-horizontal","left-vertical","right-horizontal","left-trigger","right-trigger","right-vertical"]
+    self.axisValues = dict()
     for axis in self.axisMap:
       self.axisValues[axis] = 0.0 #no pressure on joystick
     
@@ -62,21 +69,28 @@ class PS4Controller(object):
           print("%d -> %s" % (cur_joystick.get_id(),cur_joystick.get_name()))
     PS4Controller.pygame_initialization_complete = True
 
-
+ 
   def print_status(self):
     for button in self.buttonMap:
       print("%s: %d" % (button,self.buttonValues[button]))
     for axis in self.axisMap:
       print("%s: %.2f" % (axis,self.axisValues[axis]))
 
+
   def processEvent(self,event):
     if(event.type == pygame.JOYAXISMOTION):
+      val = event.value
+      wall = 0.99999999999
+      if(val<-wall):
+        val= -wall
+      elif(val>wall):
+        val=wall
       if(event.axis in [3,4]): #triggers should only go from 0 to 1
-        self.axisValues[self.axisMap[event.axis]] = (event.value+1)*0.5
+        self.axisValues[self.axisMap[event.axis]] = (val+1)*0.5
       elif(event.axis in [1,5]): #vert movement vals need flipped
-        self.axisValues[self.axisMap[event.axis]] = -event.value
+        self.axisValues[self.axisMap[event.axis]] = -val
       else:
-        self.axisValues[self.axisMap[event.axis]] = event.value
+        self.axisValues[self.axisMap[event.axis]] = val
     elif(event.type == pygame.JOYBUTTONDOWN):
       self.buttonValues[self.buttonMap[event.button]] = True
     elif(event.type == pygame.JOYBUTTONUP):
@@ -89,22 +103,27 @@ class PS4Controller(object):
 
 
   def listen_foreground(self):
-    print("beginning listen stream...")
+    if(self.thread is not None):
+      print("Don't call listen_foreground if you have listen_background running.")
+      print("This leads to performance issues (lag).")
+      print("You can call '__youcontrollervar__.stop_listening()' to kill the background listening process")
+      return -1
     while True:
       different = False
       for event in pygame.event.get():
         different = True
         self.processEvent(event)
-      #if(different):
-      #  os.system("clear")
-      #  self.print_status()
+      if(different):
+        os.system("clear")
+        self.print_status()
+
 
   def bg_proc(self):
-    while True:
+    while(not self.killRequest):
       for event in pygame.event.get():
         self.processEvent(event)
-
   def listen_background(self):
+    self.killRequest = False #assert that this thread will start
     if(self.thread is not None): #don't fork more than once
       print("Already listening to this device")
       return -1
@@ -112,14 +131,42 @@ class PS4Controller(object):
     self.thread.daemon = True
     self.thread.start()
 
+
+  def clear_vars(self):
+    for button in self.buttonMap:
+      self.buttonValues[button] = False #not pressed
+    for axis in self.axisMap:
+      self.axisValues[axis] = 0.0 #no pressure on joystick
+  def stop_listening(self):
+    if(self.thread is not None):
+      self.killRequest = True #tell bg thread to stop listening
+      print("Attempting to kill process...")
+      while(self.thread.isAlive()):
+        time.sleep(1)
+      self.thread = None
+      self.clear_vars()
+      print("Process killed")
+
+
+  def getValue(self,name):
+    if(self.thread is None):
+      print("Warning, you have not setup the event listener.")
+      return -1
+    elif(name in self.buttonMap):
+      return self.buttonValues[name]
+    elif(name in self.axisMap):
+      return self.axisValues[name]
+    else:
+      print("Query not recognized. Acceptable queries are:")
+      print(self.buttonMap)
+      print(self.axisMap)
+
+
+
 if(run_as_app):
   start = time.time()
   controller = PS4Controller(displayInfo=True)
-  controller.listen_background()
   end = time.time()
   print("setup completes in %.2f seconds" % (end-start))
-
-  while True:
-    time.sleep(0.1)
-    os.system("clear")
-    controller.print_status()
+  time.sleep(2)
+  controller.listen_foreground()
