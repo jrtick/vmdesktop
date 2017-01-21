31cf4cc3dc7408e58634e78d68e6de1f82ace964
jrtick <jrtick@andrew.cmu.edu>
2017-01-21 15:59:40 -0500

    updating aruco code

--- aruco.py
+++ aruco.py
@@ -1,27 +1,33 @@
 import numpy as np
-import cv2,cozmo
+import cv2,cozmo,time,PIL
 import cv2.aruco as aruco
 
-def myProgram(robot):
+def myProgram(robot,dict_name=aruco.DICT_5X5_100):
+  adict = aruco.Dictionary_get(dict_name)
+  params = aruco.DetectorParameters_create()
+
   robot.camera.image_stream_enabled = True
+
+  #wait until images have started
   while(True):
-    curim = robot.world.latest_image
-    if(curim is not None):
-      curim = np.array(curim.raw_image).astype(np.uint8)
-      gray = cv2.cvtColor(curim,cv2.COLOR_BGR2GRAY)
-      adict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-      params = aruco.DetectorParameters_create()
-      (corners,ids,rejected) = aruco.detectMarkers(gray,adict,parameters=params)
-      if(len(corners)):
-        print("corners is",corners)
-      im = aruco.drawDetectedMarkers(gray,corners)
+    if(robot.world.latest_image is not None):
+      break
+    else:
+      time.sleep(0.2)
 
-      curim[:,:,2] = im
-      im = curim
+  #continuously look for aruco tags
+  while(True):
+    cozmoim = robot.world.latest_image.raw_image
+    cozmoim = cozmoim.resize((cozmoim.width*2,cozmoim.height*2))
+    curim = np.array(cozmoim).astype(np.uint8)
+    gray = cv2.cvtColor(curim,cv2.COLOR_BGR2GRAY)
+    (corners,ids,rejected) = aruco.detectMarkers(gray,adict,parameters=params)
 
-      cv2.imshow('frame',im)
-      if(cv2.waitKey(1) & 0xFF == ord('q')):
-        break
+    annotated_im = aruco.drawDetectedMarkers(curim,corners,ids)
+    
+    cv2.imshow('frame',annotated_im)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+      break
 
 cozmo.robot.Robot.drive_off_charger_on_connect = False
 cozmo.run_program(myProgram)
---
+++ aruco_fsm.py
@@ -0,0 +1,55 @@
+from cozmo_fsm import *
+import numpy,cv2,time
+
+class ArucoSearch(StateNode):
+  def init(self,aruco_lib = cv2.aruco.DICT_4X4_100):
+    """user needs to specify which aruco dict they want cozmo to look for"""
+    super.__init__()
+
+    #setup aruco dictionary
+    self.aruco_lib = cv2.aruco.Dictionary_get(aruco_lib)
+    self.aruco_params = cv2.aruco.DetectorParameters_create()
+
+    #setup any necessary vars
+    self.windowName = "aruco_frame"
+
+
+  def start(self,event=None):
+    if self.running: return
+    super().start(event)
+
+    #setup cozmo image stream
+    self.robot.camera.image_stream_enabled = True
+    while(self.robot.world.latest_image is None):
+      time.sleep(0.2)
+
+    #setup opencv display window
+    cv2.namedWindow(self.windowName)
+    cv2.startWindowThread()
+
+    #run aruco detection until user presses 'q'
+    while True:
+      (_,_,im) = self.detect()
+      cv2.imshow(self.windowName,im)
+      if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+
+  def stop(self):
+    if not self.running: return
+    cv2.destroyWindow(self.windowName) #close window
+    super().stop()
+
+
+  def detect(self):
+    #grab an image
+    cozmoim = robot.world.latest_image.raw_image
+    curim = np.array(cozmoim).astype(np.uint8)
+    gray = cv2.cvtColor(curim,cv2.COLOR_BGR2GRAY)
+
+    #detect tags & annotate image
+    (corners,ids,_) = cv2.aruco.detectMarkers(gray,self.aruco_lib,parameters=self.aruco_params)
+    curim = arudo.drawDetectedMarkers(curim,corners,ids)
+
+    #return ids, bboxes, and an annotated im for display
+    return (ids,corners,curim)
